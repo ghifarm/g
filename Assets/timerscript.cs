e86f45f3cbe4237ab822b649e116a27c97822514
ghifarm <59076241+ghifarm@users.noreply.github.com>
2020-03-27 16:23:52 +0700

    third commit

---
+++ Assets/timerscript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int counter = 30;
+
+    public Text Timer;
+    void Start()
+    {
+        Timer.text = counter.ToString();
+        StartCoroutine(hitungmundur());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator hitungmundur()
+
+    {
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            counter -= 1;
+            Timer.text = counter.ToString();
+
+        }
+
+        Timer.text = "GAME OVER!!";
+    }
+
+}

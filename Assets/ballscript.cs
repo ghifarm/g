4640b8de5689c0c1a3bc70a24afdf0c35770d0d7
ghifarm <59076241+ghifarm@users.noreply.github.com>
2020-04-03 14:43:32 +0700

    commit 5

--- Assets/ballscript.cs
+++ Assets/ballscript.cs
@@ -7,15 +7,24 @@ public class ballscript : MonoBehaviour
     public int speed = 10;
     // Start is called before the first frame update
     public Rigidbody2D ball;
+    public Animator animatr;
     void Start()
     {
         ball.velocity = new Vector2(1, 1) * speed;
+        animatr.SetBool("IsMove", true);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (ball.velocity.x > 0) //move right
+        {
+            ball.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);   
+        }
+        else
+        {
+            ball.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -28,9 +37,11 @@ public class ballscript : MonoBehaviour
         IEnumerator jeda()
         {
             ball.velocity = new Vector2(0, 0);
+            animatr.SetBool("IsMove", false);
             ball.GetComponent<Transform>().position = new Vector2(0, 0);
             yield return new WaitForSeconds(1);
             ball.velocity = new Vector2(1, 1) * speed;
+            animatr.SetBool("IsMove", true);
         }
     }
 }
